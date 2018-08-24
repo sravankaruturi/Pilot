0444cd6810ce32d3c54f8bae30dde265e874191a
Sravan Karuturi <Sravan Karuturi@Home>
2018-08-24 16:10:46 -0400

    Added Geometry Shader stuff in the Combined Shader stuff.

--- Engine/GLShader.cpp
+++ Engine/GLShader.cpp
@@ -74,8 +74,9 @@ namespace piolot
 	{
 
 		// 1. retrieve the vertex/fragment source code from filePath
-		std::string vertexCode;
-		std::string fragmentCode;
+		std::string vertex_code;
+		std::string geometry_shader_code = "";
+		std::string fragment_code;
 
 		std::ifstream shader_file_stream;
 
@@ -86,7 +87,7 @@ namespace piolot
 			shader_file_stream = std::ifstream(_combinedFilePath);
 			std::string line;
 
-			std::stringstream ss[2];
+			std::stringstream ss[3];
 			int stream_index = -1;
 
 			while (getline(shader_file_stream, line))
@@ -98,14 +99,19 @@ namespace piolot
 					else if (line.find("fragment") != std::string::npos) {
 						stream_index = 1;
 					}
+					else if (line.find("geometry") != std::string::npos) {
+						stream_index = 2;
+					}
 				}
 				else {
 					ss[stream_index] << line << '\n';
 				}
 			}
 
-			vertexCode = ss[0].str();
-			fragmentCode = ss[1].str();
+			vertex_code = ss[0].str();
+			fragment_code = ss[1].str();
+
+			geometry_shader_code = ss[2].str();
 
 		}
 		catch (std::ifstream::failure& _)
@@ -113,27 +119,52 @@ namespace piolot
 			std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
 		}
 
-		const char* vShaderCode = vertexCode.c_str();
-		const char * fShaderCode = fragmentCode.c_str();
-		// 2. compile shaders
-		unsigned int vertex, fragment;
+		const char* vShaderCode = vertex_code.c_str();
+		const char * fShaderCode = fragment_code.c_str();
+
+		const char * g_shader_code;
+		if (!geometry_shader_code.empty()) {
+			g_shader_code = geometry_shader_code.c_str();
+		}
+
+		// Compile the shaders
+		unsigned int vertex, fragment, geometry;
+
 		bool success;
+
 		// vertex shader
-		vertex = glCreateShader(GL_VERTEX_SHADER);
-		glShaderSource(vertex, 1, &vShaderCode, NULL);
-		glCompileShader(vertex);
+		GL_CALL(vertex = glCreateShader(GL_VERTEX_SHADER));
+		GL_CALL(glShaderSource(vertex, 1, &vShaderCode, NULL));
+		GL_CALL(glCompileShader(vertex));
 		success = CheckCompileErrors(vertex, "VERTEX");
 		compileStatus = compileStatus && success;
+
 		// fragment Shader
-		fragment = glCreateShader(GL_FRAGMENT_SHADER);
-		glShaderSource(fragment, 1, &fShaderCode, NULL);
-		glCompileShader(fragment);
+		GL_CALL(fragment = glCreateShader(GL_FRAGMENT_SHADER));
+		GL_CALL(glShaderSource(fragment, 1, &fShaderCode, NULL));
+		GL_CALL(glCompileShader(fragment));
 		success = CheckCompileErrors(fragment, "FRAGMENT");
 		compileStatus = compileStatus && success;
+
+		if (!geometry_shader_code.empty()) {
+			// Geometry Shader
+			GL_CALL(geometry = glCreateShader(GL_GEOMETRY_SHADER));
+			GL_CALL(glShaderSource(geometry, 1, &g_shader_code, NULL));
+			GL_CALL(glCompileShader(geometry));
+			success = CheckCompileErrors(geometry, "GEOMETRY");
+			compileStatus = compileStatus && success;
+		}
+
+
 		// shader Program
 		shaderId = glCreateProgram();
 		glAttachShader(shaderId, vertex);
 		glAttachShader(shaderId, fragment);
+
+		if (!geometry_shader_code.empty()) {
+			GL_CALL(glAttachShader(shaderId, geometry));
+		}
+
 		glLinkProgram(shaderId);
 		success = CheckCompileErrors(shaderId, "PROGRAM");
 		compileStatus = compileStatus && success;
@@ -141,11 +172,14 @@ namespace piolot
 		glDeleteShader(vertex);
 		glDeleteShader(fragment);
 
+		if (!geometry_shader_code.empty()) {
+			GL_CALL(glDeleteShader(geometry));
+		}
+
 	}
 
 	bool GLShader::CheckCompileErrors(unsigned _shaderId, std::string _type)
 	{
-
 		int success;
 		char infoLog[1024];
 		if (_type != "PROGRAM")
